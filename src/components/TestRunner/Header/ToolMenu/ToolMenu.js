b1467597edac277bcff56fa31bc165d25de2555a
Martin Nicholson <recyclebing+github@gmail.com>
2019-10-06 16:38:43 +0200

    Changed ToolMenu into stateful, clickable, styled component

--- src/components/TestRunner/Header/ToolMenu/ToolMenu.js
+++ src/components/TestRunner/Header/ToolMenu/ToolMenu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import ThemeSwitcher from './ThemeSwitcher';
 import { FiSettings } from "react-icons/fi";
@@ -8,19 +8,23 @@ const propTypes = {
 };
 
 function ToolMenu(props) {
+    const [isOpen, toggleOpen] = useState(false);
+
+    const showHide = () => toggleOpen(!isOpen);
+
     return (
-        <aside aria-labelledby="tools" className="toolMenu">
+        <aside aria-labelledby="tools" className="toolMenu" onClick={showHide}>
             <FiSettings></FiSettings>
-            <div className="popup-menu">
+            <div className={'popup-menu ' + (isOpen ? '' : 'qti-visually-hidden')}>
                 <h2 id="tools" className="qti-visually-hidden">tool box & configuration</h2>
                 <h3 id="tool_title" className="qti-visually-hidden">My Test Tools</h3>
                 <ThemeSwitcher toggleTheme={props.toggleTheme}></ThemeSwitcher>
-                <button>Calculator</button>
-                <button>Highlighter</button>
+                <button disabled>Calculator</button>
+                <button disabled>Highlighter</button>
                 <h3 className="qti-visually-hidden">My Exhibits</h3>
-                <button>Exhibit: Periodic Table of Elements</button>
+                <button disabled>Exhibit: Periodic Table of Elements</button>
                 <h3 className="qti-visually-hidden">My Test Configuration</h3>
-                <button>Test Configuration</button>
+                <button disabled>Test Configuration</button>
             </div>
         </aside>
     );
