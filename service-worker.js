58805d4c470c9594cfb1e117c5ab05c75c9229d2
Martin Nicholson <recyclebing+github@gmail.com>
2019-10-15 22:36:30 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-testrunner-mockup/precache-manifest.a80cf6f3a0f7cf2061f717913cd6f98b.js"
+  "/react-testrunner-mockup/precache-manifest.642ce1d9510ee4fb9875205f593b7f51.js"
 );
 
 self.addEventListener('message', (event) => {
