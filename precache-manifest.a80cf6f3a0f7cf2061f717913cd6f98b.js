b9db883c2ece1235bcf5898177931fb63f7c493e
Martin Nicholson <recyclebing+github@gmail.com>
2019-10-15 20:53:28 +0200

    Updates

---
+++ precache-manifest.a80cf6f3a0f7cf2061f717913cd6f98b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fdb27d09da50b2ee8d90a91978d6159",
+    "url": "/react-testrunner-mockup/index.html"
+  },
+  {
+    "revision": "7f590b8eafaf04bdc2d6",
+    "url": "/react-testrunner-mockup/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "b1e80e44b51a76cd14ef",
+    "url": "/react-testrunner-mockup/static/css/main.f894b03b.chunk.css"
+  },
+  {
+    "revision": "7f590b8eafaf04bdc2d6",
+    "url": "/react-testrunner-mockup/static/js/2.e1ba08a0.chunk.js"
+  },
+  {
+    "revision": "b1e80e44b51a76cd14ef",
+    "url": "/react-testrunner-mockup/static/js/main.e6460274.chunk.js"
+  },
+  {
+    "revision": "36dad6be7f8a6392bd7e",
+    "url": "/react-testrunner-mockup/static/js/runtime-main.d0edd4c5.js"
+  }
+]);
